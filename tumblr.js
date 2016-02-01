e43a6a12d9d4be9815962751f3d0278b3b3ae39d
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-02-02 00:56:01 +0100

    Add a bit of overlay on click of a button

--- tumblr.js
+++ tumblr.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+    $('.inner-container').hide()
 
+    $('.home button').click(function(e) {
+        e.preventDefault()
+        $(this).hide();
+        $('.inner-container').show();
+    });
 });
