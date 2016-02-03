46102538a0540bd655a8687c1f968656f4d43edc
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-02-03 22:39:34 +0100

    Set home div properly

--- tumblr.js
+++ tumblr.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
     $('.inner-container').hide()
 
+    // Show inner container on click of home button
     $('.home button').click(function(e) {
         e.preventDefault()
-        $(this).hide();
+        $('.home').hide();
         $('.inner-container').show();
+    });
+
+    // Hide inner container on when you click outside
+    $(document).mouseup(function (e){
+        var container = $('.inner-container');
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.hide();
+            $('.home').show();
+        }
     });
 });
